e746587c5647717232998df8c2625e470786da9a
elijahmandeville <elijahmandeville@gmail.com>
2020-04-16 18:09:28 -0600

    wrapped up MVP

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-const express = require('express');
+const express = require("express");
 
+const userRouter = require("./users/userRouter");
+const cors = require("cors");
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
 //custom middleware
 
-function logger(req, res, next) {}
+server.use(express.json());
+server.use(cors());
+server.use(logger);
+
+server.use("/users", userRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "something went wrong",
+  });
+});
+
+function logger(req, res, next) {
+  console.log(
+    `${new Date().toISOString()} ${req.ip}, ${req.method}, ${req.url}`
+  );
+  next();
+}
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
